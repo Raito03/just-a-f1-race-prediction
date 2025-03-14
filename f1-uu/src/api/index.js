4eaf184e7217a35a773043eac7e5a323481baca5
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-03-14 22:37:22 +0530

    api Url Change

--- f1-uu/src/api/index.js
+++ f1-uu/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/api";
+// const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://just-a-f1-race-prediction.onrender.com/api";
 
 export const getData = async () => {
   try {
